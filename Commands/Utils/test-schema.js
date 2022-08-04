5d8536e145c2ef726e1893f1743f192e446d7e56
Adrian9540YTexe <adrian9540gamez@gmail.com>
2022-08-04 15:14:10 -0700

    yay

--- Commands/Utils/test-schema.js
+++ Commands/Utils/test-schema.js
@@ -19,6 +19,33 @@ const User = new mongoose.Schema({
         required: true,
         default: "0",
     },
+    levelranking: {
+        level: {
+            type: String,
+            required: true,
+            default: "0",
+        },
+        xp: {
+            type: String,
+            required: true,
+            default: "0",
+        },
+        bg: {
+            type: String,
+            required: true,
+            default: "",
+        },
+        xpneeded: {
+            type: String,
+            required: true,
+            default: "100",
+        },
+        total: {
+            type: String,
+            required: true,
+            default: "100",
+        },
+    },
     cooldowns: {
         daily: {type: Date}
     }
