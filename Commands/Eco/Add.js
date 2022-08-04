5a4b276285bf8567959c0aa9d7ba54976fdacb56
Adrian9540YTexe <adrian9540gamez@gmail.com>
2022-08-04 02:40:20 -0700

    nooo

--- Commands/Eco/Add.js
+++ Commands/Eco/Add.js
@@ -9,10 +9,10 @@ module.exports = {
             const user = message.mentions.users.first() || message.author;
             const userData = await User.findOne({id: user}) || new User({id: user})
             const embed = new MessageEmbed({ color: "BLUE"})
-            userData.bal = ExpantaNum.add(`${userData.bal}`,args[1]).toStringWithDecimalPlaces(2)
+            userData.bal = ExpantaNum.add(userData.bal,args[1]).toStringWithDecimalPlaces(2)
             return message.channel.send({
              embeds: [
-                 embed.setDescription('Added ' + ExpantaNum(args[1]).toStringWithDecimalPlaces(2) + " to " + `${user.username}`)
+                 embed.setDescription('Added ' + ExpantaNum(args[1]).toString() + " to " + `${user.username}`)
                 ]
             })
      
