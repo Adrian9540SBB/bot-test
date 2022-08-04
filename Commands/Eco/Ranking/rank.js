3c4880cdbc33f3eb8ec9ec0aca9196fd0b8dbe24
Adrian9540YTexe <adrian9540gamez@gmail.com>
2022-08-04 16:09:06 -0700

    NOa

--- Commands/Eco/Ranking/rank.js
+++ Commands/Eco/Ranking/rank.js
@@ -1,7 +1,12 @@
+console.log("lol")
 const fs = require("fs");
+console.log("lol")
 const {MessageEmbed, MessageAttachment, ReactionUserManager} = require("discord.js")
-const { createCanvas, loadimage, loadImage } = require("canvas")
+console.log("lol")
+const { createCanvas, loadImage } = require("canvas")
+console.log("lol")
 const { User } = require("../../Utils/test-schema.js")
+console.log("lol")
 const ExpantaNum = require("../ExpantaNum.js")
 console.log("lol")
 module.exports = {
