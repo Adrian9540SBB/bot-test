5eedf852a6fe9e38c080cf4ec0dc9ea52cdba4fd
Adrian9540YTexe <adrian9540gamez@gmail.com>
2022-08-04 00:40:11 -0700

    noyes

--- Commands/Eco/Add.js
+++ Commands/Eco/Add.js
@@ -6,15 +6,20 @@ module.exports = {
     name: "add",
     description: "lol",
     execute(message, args, commandName, client, Discord) {
-       const user = message.guild.members.cache.get(target.id);
-       const userData = await User.findOne({id: user}) || new User({id: user})
-       const embed = new MessageEmbed({ color: "BLUE"})
-       userData.bal = ExpantaNum.add(args[0])
-       return message.reply({
-        embeds: [
-            embed.setDescription('Added' + ExpantaNum(args[0]).toStringWithDecimalPlaces(2) + "to" + user.username)
-           ]
-       })
+        run: async(message) => {
+            const target = message.mentions.users.first() || message.author;
+            const user = message.guild.members.cache.get(target.id);
+            const userData = await User.findOne({id: user}) || new User({id: user})
+            const embed = new MessageEmbed({ color: "BLUE"})
+            userData.bal = ExpantaNum.add(args[0])
+            return message.channel.send({
+             embeds: [
+                 embed.setDescription('Added' + ExpantaNum(args[0]).toStringWithDecimalPlaces(2) + "to" + user.username)
+                ]
+            })
+     
+         
+        }
 
     }
-}
+} 
--- Commands/Eco/Bal.js
+++ Commands/Eco/Bal.js
@@ -5,19 +5,21 @@ module.exports = {
     name: "bal",
     description: "Your balance",
     execute(message, args, commandName, client, Discord) {
-        const target = message.mentions.users.first() || message.author;
-        const user = message.guild.members.cache.get(target.id);
-        const userData = await User.findOne({id: user}) || new User({id: user})
-        const balanceEmbed = new MessageEmbed()
-        .setTitle(`${user.username}'s balance`)
-        .setDescription("Wallet and bank details of requested user")
-        .setColor("GREEN")
-        .setThumbnail(user.displayAvatarURL())
-        .addField("- Wallet", `**\` ${userData.bal} \`**`, true)
-        .addField("- Bank", `**\` ${userData.bank} \`**`, true)
-
-        return message.channel.send({
-            embeds: [ balanceEmbed ]
-        })
+        run: async(message) => {
+            const target = message.mentions.users.first() || message.author;
+            const user = message.guild.members.cache.get(target.id);
+            const userData = User.findOne({id: user}) || new User({id: user})
+            const balanceEmbed = new MessageEmbed()
+            .setTitle(`${user.username}'s balance`)
+            .setDescription("Wallet and bank details of requested user")
+            .setColor("GREEN")
+            .setThumbnail(user.displayAvatarURL())
+            .addField("- Wallet", `**\` ${userData.bal} \`**`, true)
+            .addField("- Bank", `**\` ${userData.bank} \`**`, true)
+    
+            return message.channel.send({
+                embeds: [ balanceEmbed ]
+            })
+        }
     }
 }
