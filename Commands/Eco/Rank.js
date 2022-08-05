b6e42b9d0ecc7381cba0191da1de3cbc7d26f69d
Adrian9540YTexe <adrian9540gamez@gmail.com>
2022-08-04 21:40:42 -0700

    lol

--- Commands/Eco/Rank.js
+++ Commands/Eco/Rank.js
@@ -1,22 +1,25 @@
 
 const fs = require("fs");
-const {MessageEmbed, MessageAttachment, Client} = require("discord.js")
+const {MessageEmbed, MessageAttachment} = require("discord.js")
 
 const { createCanvas, loadImage } = require("canvas")
 
 const { User } = require("../Utils/test-schema.js")
 
-const ExpantaNum = require("./ExpantaNum.js");
-    Client.on('messageCreate', async message => {
-    const prefix = process.env.TOKEN
-    xps(message)
-    if (message.content.toLowerCase() === `${prefix}rank`) {
+const ExpantaNum = require("./ExpantaNum.js")
+
+module.exports = {
+    name: "rank",
+    description: "yes",
+    async execute(message, args, commandName, client, Discord) {
         const user = message.mentions.users.first() || message.author;
-        const userData = User.findOne({id: user}) || new User({id: user})
-        const backgroundranks = `${userData.levelranking.backgroundrank}`
+        const userData = await User.findOne({id: user}) || new User({id: user})
+        xps(message)
+        const member = message.mentions.users.first() || message.author;
         const level = `${userData.levelranking.level}`
         const xp = `${userData.levelranking.xp}`
         const xpneed = ExpantaNum(level * 2 * 250 + 250)
+        let backgroundranks = userData.levelranking.backgroundrank
 
         if (!backgroundranks) {
             console.log("leeol")
@@ -52,7 +55,7 @@ const ExpantaNum = require("./ExpantaNum.js");
     
             ctx.fillStyle = "#b3ff00"
             ctx.globalAlpha= 0.6
-            ctx.fillRect(200,216,((ExpantaNum(((100 / (level * 2 * 250 + 250)) * currentXP) * 7.5))),50) // Uses ExpantaNum
+            ctx.fillRect(200,216,((ExpantaNum.div(ExpantaNum.mul(ExpantaNum.mul(ExpantaNum.add(ExpantaNum.mul(ExpantaNum.mul(2,level),250),250),xp),7.5),100))),50) // Uses ExpantaNum
             ctx.fill()
             ctx.globalAlpha = 1
     
@@ -94,27 +97,30 @@ const ExpantaNum = require("./ExpantaNum.js");
     
             const avatar = await loadImage(user.displayAvatarURL({ format: 'jpg' }))
             ctx.drawImage(avatar,40,40,250,250)
+            const image = await loadImage(canvas({ format: 'png'}))
             
             const attachment = new Discord.MessageAttachment(canvas.toBuffer(),'rank.png')
-            message.channel.send({
+            message.channel.reply({
                 files: [attachment]
             })
-        }
-        
-        function xps(message) {
-            if(!userData) return
-            if(message.author.bot) return
-            const randomXP = Math.floor(Math.random() * 150) + 50
-            userData.xp = ExpantaNum.add(userData.levelranking.xp,randomXP).toString()
-            const level = `${userData.levelranking.level}`
-            const xp = `${userData.levelranking.xp}`
-            const xpneed = ExpantaNum(level * 2 * 250 + 250).toString()
 
-            if (ExpantaNum.gte(ExpantaNum(xp),ExpantaNum(xpneed))) {
-                const levelnew = ExpantaNum.add(`${userData.levelranking.level}`,1).toString()
+
+            
+            function xps(message) {
+                if(message.author.bot) return
+                const randomXP = Math.floor(Math.random() * 150) + 50
+                userData.xp = ExpantaNum(ExpantaNum.add(userData.levelranking.xp,randomXP)).toString()
+                const level = `${userData.levelranking.level}`
+                const xp = `${userData.levelranking.xp}`
+                const xpneed = ExpantaNum(level * 2 * 250 + 250).toString()
+    
+                if (ExpantaNum.gte(ExpantaNum(xp),ExpantaNum(xpneed))) {
+                    const levelnew = ExpantaNum.add(`${userData.levelranking.level}`,1).toString()
+                }
             }
-        }
-        userData.save()
-    })   
+            userData.save()
+            
+        }      
+    }
 
     
