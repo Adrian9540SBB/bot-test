346c709103370ef98f2ba6608894e125e75c35aa
Adrian9540YTexe <adrian9540gamez@gmail.com>
2022-08-07 22:21:36 -0700

    e

--- Commands/Eco/Rank.js
+++ Commands/Eco/Rank.js
@@ -18,7 +18,7 @@ module.exports = {
         if (!backgroundranks) {
             console.log("leeol")
             const FBGRL = "https://png.pngtree.com/png-clipart/20200701/original/pngtree-abstract-star-space-transparency-background-png-image_5439546.jpg"
-            userData.levelranking.backgroundrank = FBGRL
+            const response = User.findOneAndUpdate({id: user},$set({FBGRL}))
             return
         }
     
@@ -28,7 +28,7 @@ module.exports = {
             if(!backgroundimage || backgroundimage === null) {
                 console.log("aaaaa")
                 const FBGRL = "https://png.pngtree.com/png-clipart/20200701/original/pngtree-abstract-star-space-transparency-background-png-image_5439546.jpg"
-                userData.levelranking.backgroundrank = FBGRL
+                const response = User.findOneAndUpdate({id: user},$set({FBGRL}))
                 message.reply("Try Again")
                 return
             }
